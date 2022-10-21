20536e2e212357e9db064357e04d842d5fc5b3c0
YamilLazzariDicsys <yamil.lazzari@dicsys.com>
2022-10-21 15:50:11 -0300

    Add html css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private socket: Socket) {
+    socket.fromEvent('new_message').subscribe((msj: any) => {
+      console.log('llego un mensaje ', msj)
+      this.message=true;
+      setTimeout(() => {
+        this.message=false;
+      }, 1000);
+    });
+  }
+
   title = 'Angular';
+  message = false;
+
+  private _room$ = new BehaviorSubject<string | null>('UNO');
+
+
+  ngOnInit(): void {
+    this.socket.emit('event_message', '🎈 Dato enviado desde el front');
+    this.joinRoom();
+  }
+
+  sendMessage(msg: string) {
+    this.socket.emit('event_join', msg);
+  }
+
+  getMessage() {
+    return this.socket.fromEvent('message').pipe(map((data) => data));
+  }
+
+  leaveRoom(){
+    this.socket.emit('event_leave', this._room$.getValue() );
+  }
+
+  joinRoom(){
+    this.socket.emit('event_join', this._room$.getValue() );
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const config: SocketIoConfig = { url: 'https://yamil.loca.lt', options: {} };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
